b7f9cc85edd5e52a762933aed15845988bc1b024
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-12-17 21:22:26 +0530

    fixed import

--- src/components/products/ProductsPage.jsx
+++ src/components/products/ProductsPage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./productsPage.css";
-import chemicalVideo from "../../assets/video/chemical-produc.gif";
 import { VscHome } from "react-icons/vsc";
 import { IoIosArrowForward } from "react-icons/io";
 import ProductList from "../productlist/ProductList";
 import { Link } from "react-router-dom";
 
-export default function productsPage() {
+export default function ProductsPage() {
   return (
     <div className="products-page-wrapper">
       <div className="top-marks">
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../components/Navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
-import ProductsPage from "../components/products/productsPage";
+import ProductsPage from "../components/products/ProductsPage";
 
 export default function Products() {
   return (
