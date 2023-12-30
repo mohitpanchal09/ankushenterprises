afa052be5bc6c2006fbf5fec0de633f91cd91133
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-12-30 20:51:50 +0530

    industries fixed

--- src/components/popularproducts/PopularProducts.jsx
+++ src/components/popularproducts/PopularProducts.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./PopularProducts.css";
 import data from "../../products";
+import { Link } from "react-router-dom";
 
 export default function PopularProducts() {
-  const arr = data.slice(0, 6);
+  const arr = data.slice(6, 12);
   return (
     <div className="popular-products">
       <h1>
@@ -16,7 +17,39 @@ export default function PopularProducts() {
               <div className="slick-inner-card">
                 <div className="img-container-prod">
                   <img src={item.img} alt="" />
-                  <div className="red-bg">{item.title}</div>
+                  <div className="red-bg">
+                    {item.title}
+                    <div className="popular-products-button">
+                      <Link to={`/product/${item.id}`}>
+                        <button
+                          style={{
+                            backgroundColor: "#ED1D2B",
+                            color: "white",
+                            borderRadius: "30px",
+                            border: "none",
+                            fontSize: "12px",
+                            padding: "5px",
+                          }}
+                        >
+                          View Product
+                        </button>
+                      </Link>
+                      <Link to={`/contact-us`}>
+                        <button
+                          style={{
+                            backgroundColor: "white",
+                            color: "black",
+                            borderRadius: "30px",
+                            border: "none",
+                            fontSize: "12px",
+                            padding: "5px",
+                          }}
+                        >
+                          Enquiry now
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/productlist/ProductList.jsx
+++ src/components/productlist/ProductList.jsx
@@ -10,24 +10,41 @@ import { useLocation } from "react-router-dom";
 export default function ProductList(props) {
   const { category } = props;
 
-  const productsPerPage = 6;
-  const [currentPage, setCurrentPage] = useState(0);
+  // const productsPerPage = 6;
+  // const [currentPage, setCurrentPage] = useState(0);
   const filteredProducts = category
     ? data.filter((item) => item.category == category)
     : data;
 
-  const indexOfLastProduct = (currentPage + 1) * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = filteredProducts.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct
-  );
+  // const indexOfLastProduct = (currentPage + 1) * productsPerPage;
+  // const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  // const currentProducts = filteredProducts.slice(
+  //   indexOfFirstProduct,
+  //   indexOfLastProduct
+  // );
 
-  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+  // const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
 
-  const handlePageClick = (filteredProducts) => {
-    setCurrentPage(filteredProducts.selected);
-  };
+  // const handlePageClick = (filteredProducts) => {
+  //   setCurrentPage(filteredProducts.selected);
+  // };
+  if (filteredProducts.length === 0) {
+    return (
+      <div
+        className="product-list-wrapper"
+        style={{
+          height: "50vh",
+        }}
+      >
+        <h1>
+          Our <span>Products</span>
+        </h1>
+        <p style={{ textAlign: "center", margin: "70px 0" }}>
+          No products found.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="product-list-wrapper">
@@ -35,7 +52,7 @@ export default function ProductList(props) {
         Our <span>Products</span>
       </h1>
       <div className="products-wrapper">
-        {currentProducts.map((item, index) => (
+        {filteredProducts.map((item, index) => (
           <div className="product" key={index}>
             <img src={item.img} alt="" />
             <p>{item.title}</p>
@@ -53,7 +70,7 @@ export default function ProductList(props) {
           </div>
         ))}
       </div>
-      <ReactPaginate
+      {/* <ReactPaginate
         pageCount={totalPages}
         pageRangeDisplayed={3}
         marginPagesDisplayed={2}
@@ -62,7 +79,7 @@ export default function ProductList(props) {
         activeClassName={"active"}
         previousLabel={<BsChevronLeft />}
         nextLabel={<BsChevronRight />}
-      />
+      /> */}
     </div>
   );
 }
