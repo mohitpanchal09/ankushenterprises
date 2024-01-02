ae3c51353b75a98fb37358c86f2d2da0ff39464b
mohitpanchal09 <panchalmohitg2002@gmail.com>
2024-01-02 15:29:00 +0530

    added mail service

--- src/components/contact-us/ContactUsComp.jsx
+++ src/components/contact-us/ContactUsComp.jsx
@@ -6,8 +6,13 @@ import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineMail } from "react-icons/ai";
 import { LiaPhoneVolumeSolid } from "react-icons/lia";
 import map from "../../assets/mapImage.png";
+import emailjs from "@emailjs/browser";
 
 export default function ContactUsComp() {
+  const [loading, setLoading] = useState(false);
+  const serviceId = "service_1vz2zz9";
+  const templateId = "template_u9bgf0g";
+  const publicKey = "KVrN2AqOkPLV4NAmL";
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,17 +26,41 @@ export default function ContactUsComp() {
     });
   };
   const handleClick = (e) => {
-    console.log(formData);
-    toast("Your message has been sent", {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
+    e.preventDefault();
+    const form = document.getElementById("yourFormId");
+    setLoading(true);
+    emailjs
+      .sendForm(serviceId, templateId, form, publicKey)
+      .then(
+        (result) => {
+          toast("Your message has been sent", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        },
+        (error) => {
+          console.error("Error sending email:", error);
+          toast("Error sending your message. Please try again later.", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      )
+      .finally(() => {
+        setLoading(false);
+      });
     setFormData({
       name: "",
       email: "",
@@ -81,43 +110,50 @@ export default function ContactUsComp() {
             Do you fancy saying hi to me or you want to get started with your
             project and you need my help? Feel free to contact me.
           </p>
-          <div className="contact-input-line-1">
+          <form id="yourFormId">
+            <div className="contact-input-line-1">
+              <input
+                type="text"
+                placeholder="your name"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <input
+                type="email"
+                placeholder="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
             <input
               type="text"
-              placeholder="your name"
-              id="name"
-              value={formData.name}
+              placeholder="subject"
+              id="subject"
+              name="subject"
+              value={formData.subject}
               onChange={handleChange}
             />
             <input
-              type="email"
-              placeholder="email"
-              id="email"
-              value={formData.email}
+              type="text"
+              placeholder="message"
+              id="message"
+              name="message"
+              value={formData.message}
               onChange={handleChange}
             />
-          </div>
-          <input
-            type="text"
-            placeholder="subject"
-            id="subject"
-            value={formData.subject}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            placeholder="message"
-            id="message"
-            value={formData.message}
-            onChange={handleChange}
-          />
-          <button
-            className="enquiry-button"
-            style={{ height: "35px", width: "150px", fontSize: "14px" }}
-            onClick={handleClick}
-          >
-            Send Message
-          </button>
+            <button
+              className="enquiry-button"
+              style={{ height: "35px", width: "150px", fontSize: "14px" }}
+              onClick={handleClick}
+              disabled={loading}
+            >
+              {loading ? "Sending..." : "Send Message"}
+            </button>
+          </form>
           <ToastContainer containerId="an id" draggable={false} />
         </div>
       </div>
